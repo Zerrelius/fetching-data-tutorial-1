53d30ebff04f30a328a5672060b703138c721b57
Zerrelius <Jeremy.Held@web.de>
2024-12-20 15:56:24 +0100

    Added fetching Method for titles

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const imageList = document.getElementById('image-list');
 // Define an array for users
 let users = []; 
 let images = []; // to be filled with images from api endpoint https://jsonplaceholder.typicode.com/photos
+let titles = [];
 
 // define async function to fetch users data
 async function fetchUserData(){
@@ -57,9 +58,49 @@ function renderImages(){
         const imageItem = document.createElement('img');
         console.log(imageItem);
         imageItem.setAttribute('src', image.url)
-        imageList.appendChild(imageItem)
+        imageList.appendChild(imageItem);
 
     });
+};
+
+function renderTitles(){
+    titles.forEach((title => {
+        const imageTitle = document.createElement('p'); 
+        imageTitle.innerText= title.text;
+        console.log(imageTitle); 
+        imageList.appendChild(imageTitle);
+    }))
 }
+
+async function fetchAlbumTitle() {
+    const url = "https://jsonplaceholder.typicode.com/albums";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        console.log("Json Daten geladen!")
+
+        json.forEach((album) => {
+            const newData = {
+                id: Date.now(),
+                text: album.title
+            }
+            console.log(newData);
+
+            titles.push(newData);
+        });
+        
+        renderTitles();
+
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+fetchAlbumTitle();
 
 // showUsers();
